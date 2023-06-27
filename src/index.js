c5f2614eafcfb3d09cabcfc37885fc79fdbcaba1
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-27 09:43:07 +0500

    error handling uchun

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const config = require('./shared/config');
+const handleError = require('./shared/errors/handle');
 const userRoute = require('./modules/users/api');
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.use(express.json());
 
 app.use(userRoute);
+
+app.use(handleError);
 
 app.listen(config.port, () => {
   console.log(`Server ${config.port}-portda ishlayapti.`);
