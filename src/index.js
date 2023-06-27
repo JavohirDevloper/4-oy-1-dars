1cd12ceeaa6506908bf69113d32596e2e8cec934
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-27 09:34:31 +0500

    retrieve users

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const config = require('./shared/config');
+const userRoute = require('./modules/users/api');
 
 const app = express();
 
 app.use(express.json());
+
+app.use(userRoute);
 
 app.listen(config.port, () => {
   console.log(`Server ${config.port}-portda ishlayapti.`);
---
+++ src/modules/users/show-user.js
@@ -0,0 +1,12 @@
+const db = require('../../db');
+
+const showUser = async ({ id }) => {
+  const user = await db('users')
+    .where({ id })
+    .select('id', 'first_name', 'last_name', 'username', 'updated_at', 'created_at')
+    .first();
+
+  return { data: user };
+};
+
+module.exports = showUser;
