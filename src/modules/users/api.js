18c97abea4bdf1f78e81671aaa002064a83561d6
MirzayevOrzu <mirzayevorzu127@gmail.com>
2023-06-27 10:27:02 +0500

    validate show user

--- src/modules/users/api.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { getUsers, getUser } = require('./controllers');
-
-const router = express.Router();
-
-router.get('/users', getUsers);
-router.get('/users/:id', getUser);
-
-module.exports = router;
